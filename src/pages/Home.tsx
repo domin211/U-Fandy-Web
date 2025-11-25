0075cc69c244212aa0351eb9ec75286eac0a0d8f
Dominik Závodný <99141033+domin211@users.noreply.github.com>
2025-11-25 16:15:53 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import Hero from '../components/Hero';
 import About from '../components/About';
 import RestaurantSection from '../components/Restaurant';
 import WellnessSection from '../components/Wellness';
+import Testimonials from '../components/Testimonials';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <RestaurantSection />
       <WellnessSection />
+      <Testimonials />
     </div>
   );
 }
